187279a331c8c993de68c5568297697f15617da7
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-01-17 12:06:21 +0530

    initial push

---
+++ projects/bankist-app/src/assets/js/component.js
@@ -0,0 +1,9 @@
+const openModal = function () {
+  modal.classList.remove('visually-hidden');
+  overlay.classList.remove('visually-hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('visually-hidden');
+  overlay.classList.add('visually-hidden');
+};
