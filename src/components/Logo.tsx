b9c8347c776be0a0c014c68011bbc9a35341f361
Lakshmikanta Patra <33117526+frontendfixer@users.noreply.github.com>
2023-09-04 21:17:10 +0530

    make large variant of logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import { twJoin } from 'tailwind-merge';
 
 import { ReactComponent as LogoSvg } from '../assets/images/logo.svg';
 
-const Logo = () => {
+const Logo = ({ large }: { large?: boolean }) => {
   return (
     <Link to="/">
-      <LogoSvg className="w-[7.5rem] text-white" />
+      <LogoSvg
+        className={twJoin('text-white', large ? 'w-48' : 'w-[7.5rem]')}
+      />
     </Link>
   );
 };
