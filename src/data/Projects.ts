f66e84a01dc13c3eb6e27cb9a7e6fb528fa8aadc
Lakshmikanta Patra <33117526+frontendfixer@users.noreply.github.com>
2023-09-05 09:46:32 +0530

    add features projects

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,9 +1,15 @@
 import demoImg from '/demo.webp';
 
+import crwnClothing from './screenshot/crwn-clothing.webp';
+import elctricSkateboards from './screenshot/electric-skateboards-USA.webp';
+import forkifyRecipe from './screenshot/forkify-recipe-js.webp';
+import openTable from './screenshot/open-table.webp';
+import adminDashboard from './screenshot/react-admin-dashboard.webp';
+
 export type ProjectType = {
   id: number;
   title: string;
-  description: string;
+  description?: string;
   image: string;
   repo: string;
   url: string;
@@ -19,12 +25,19 @@ const Projects: ProjectType[] = [
     id: 1,
     title: 'Crwn Clothing Online Store',
     description: 'This is a project 1',
-    image: demoImg,
-    repo: 'https://github.com',
-    url: 'https://www.google.com',
+    image: crwnClothing,
+    repo: 'https://github.com/frontendfixer/crwn-clothing-redux',
+    url: 'https://crwn-clothing-redux.vercel.app',
     development: {
-      language: ['react', 'typescript', 'HTML', 'CSS', 'redux'],
-      tools: ['react', 'typescript'],
+      language: [
+        'firebase',
+        'react',
+        'typescript',
+        'HTML',
+        'CSS',
+        'redux',
+        'styled_components',
+      ],
     },
     featured: true,
   },
@@ -32,9 +45,9 @@ const Projects: ProjectType[] = [
     id: 2,
     title: 'Open Table | Book a Table at Any Restaurant',
     description: 'This is a project 2',
-    image: demoImg,
-    repo: 'https://github.com',
-    url: 'https://www.google.com',
+    image: openTable,
+    repo: 'https://github.com/frontendfixer/OpenTable-NextJS-App',
+    url: 'https://open-table-next-js-app.vercel.app',
     development: {
       language: [
         'nextjs',
@@ -47,56 +60,42 @@ const Projects: ProjectType[] = [
         'HTML',
         'CSS',
       ],
-      tools: ['react', 'typescript'],
     },
     featured: true,
   },
   {
     id: 3,
-    title: 'Open Table | Book a Table at Any Restaurant',
+    title: 'Admin Dashboard | Build with React',
     description: 'This is a project 2',
-    image: demoImg,
-    repo: 'https://github.com',
-    url: 'https://www.google.com',
+    image: adminDashboard,
+    repo: 'https://github.com/frontendfixer/react-admin-ui',
+    url: 'https://react-admin-dashboard-self.vercel.app/',
     development: {
-      language: [
-        'nextjs',
-        'react',
-        'typescript',
-        'redux',
-        'MUI',
-        'prisma',
-        'postgreSQL',
-        'HTML',
-        'CSS',
-      ],
-      tools: ['react', 'typescript'],
+      language: ['react', 'typescript', 'recharts', 'MUI', 'HTML', 'CSS'],
     },
     featured: true,
   },
   {
     id: 4,
-    title: 'Project 4',
+    title: 'Forkify | Search Recipe with Ingredients',
     description: 'This is a project 4',
-    image: demoImg,
-    repo: 'https://github.com',
-    url: 'https://www.google.com',
+    image: forkifyRecipe,
+    repo: 'https://github.com/frontendfixer/forkify-recipe-js',
+    url: 'https://forkify-recipe-js.vercel.app/#5ed6604691c37cdc054bd00c',
     development: {
-      language: ['react', 'typescript', 'HTML', 'CSS', 'redux'],
-      tools: ['react', 'typescript'],
+      language: ['javascript', 'SASS', 'MVP_Architecture', 'HTML', 'CSS'],
     },
     featured: true,
   },
   {
     id: 5,
-    title: 'Project 5',
+    title: 'Electric Skateboards USA',
     description: 'This is a project 5',
-    image: demoImg,
-    repo: 'https://github.com',
-    url: 'https://www.google.com',
+    image: elctricSkateboards,
+    repo: 'https://github.com/frontendfixer/electric-skateboards-USA',
+    url: 'https://frontendfixer.github.io/electric-skateboards-USA/',
     development: {
-      language: ['react', 'typescript', 'HTML', 'CSS', 'redux'],
-      tools: ['react', 'typescript'],
+      language: ['javascript', 'SASS', 'Static HTML', 'HTML', 'CSS'],
     },
     featured: true,
   },
