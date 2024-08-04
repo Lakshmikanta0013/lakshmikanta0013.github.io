f9fb17e86c7e56d34d969817eddc96b93f651484
Lakshmikanta Patra <frontendfixer@gmail.com>
2024-08-04 18:14:01 +0530

    feat: improve intro paragraph

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,12 +5,11 @@ const Intro = () => {
         Hi, I’m Lakshmikanta. Nice to meet you.
       </h2>
       <p className="mx-auto max-w-[50ch] text-lg sm:text-center">
-        Enthusiastic and dedicated Frontend Web Developer with a strong
-        foundation in React, JavaScript, PHP, HTML5 and CSS/SASS. Eager to
-        contribute creativity and technical skills to dynamic web development
-        projects. Committed to continuous learning and staying updated with the
-        latest industry trends. I’m passionate about building beautiful and
-        functional websites and applications.
+        Passionate about creating beautiful and functional websites and
+        applications, with a solid foundation in Next.js, TypeScript, HTML5,
+        CSS3, PHP, and MySQL. Eager to leverage creativity and technical skills
+        to contribute to dynamic web development projects. Committed to
+        continuous learning and staying updated with the latest industry trends.
       </p>
     </div>
   );
