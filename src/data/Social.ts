506edfdf62b97799535886446aa173a84a088e9e
Lakshmikanta Patra <frontendfixer@gmail.com>
2024-12-24 19:54:33 +0530

    fix: update social media links to new URLs

--- src/data/Social.ts
+++ src/data/Social.ts
@@ -17,25 +17,25 @@ export const socialLinks: SocialLink[] = [
   {
     id: 1,
     name: 'GutHub',
-    url: 'https://github.com/frontendfixer',
+    url: 'https://git.new/frontendfixer',
     icon: faGithub,
   },
   {
     id: 2,
     name: 'LinkedIn',
-    url: 'https://linkedin.com/in/lakshmikanta',
+    url: 'https://dub.sh/lakshmikanta-linkedin',
     icon: faLinkedin,
   },
   {
     id: 3,
     name: 'Instagram',
-    url: 'https://instagram.com/frontendfixer',
+    url: 'https://dub.sh/frontendfixer-insta',
     icon: faInstagram,
   },
   {
     id: 4,
     name: 'Twitter',
-    url: 'https://twitter.com/frontendfixer',
+    url: 'https://dub.sh/frontendfixer-x',
     icon: faTwitter,
   },
 ];
